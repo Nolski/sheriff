ba5c2d0e3cb1cbf436e786cc73cd12956f1705d9
Mike Nolan <me@michael-nolan.com>
2018-09-24 17:37:39 +0300

    Add migration script

---
+++ src/scripts/populate_mongo.py
@@ -0,0 +1,63 @@
+import falcon
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+from mongoengine import connect, DynamicDocument, StringField
+from mongoengine.errors import NotUniqueError
+
+from typing import Tuple
+
+connect('sheriff', host='mongodb', port=27017)
+
+class JobOpening(DynamicDocument):
+    case_id = StringField(required=True, unique=True)
+    job_id = StringField(required=True, unique=True)
+
+class JobSeeker(DynamicDocument):
+    case_id = StringField(required=True, unique=True)
+
+COMMCARE_USERNAME = 'michael.nolan@rescue.org'
+COMMCARE_PASSWORD = 'k[YfqcEzdCZ42TnDv7&XmMbbL>idTY]yHLJ^kw3qk7Rct;EQT@2XV23Di{oL4*j'
+CASES_URL = 'https://www.commcarehq.org/a/billy-excerpt/api/v0.5/case/'
+
+def create_cases(next_params: str, n: int, CaseClass: DynamicDocument) -> Tuple:
+    print(f"Getting cases from {CASES_URL}{next_params}")
+    resp = requests.get(
+        CASES_URL + next_params,
+        auth=HTTPBasicAuth(COMMCARE_USERNAME, COMMCARE_PASSWORD)
+    )
+    cases = json.loads(resp.content)
+    for c in cases['objects']:
+        params = {**c['properties'], 'case_id': c['case_id']}
+        case = CaseClass(**params)
+        print(f"Creating case {case.case_id}...")
+        try:
+            case.save()
+        except NotUniqueError as e:
+            print(f"{c['case_id']} already exists, skipping...")
+        print("saved")
+        n += 1
+
+    return (resp, n)
+
+
+
+def import_cases(case_type: str, CaseClass: DynamicDocument) -> None:
+    limit_offset = '?limit=20&offset=0'
+    case_type = f"&type={case_type}"
+    n = 0
+    resp, n = create_cases(limit_offset + case_type, n, CaseClass)
+
+    next_params = json.loads(resp.content)['meta']['next']
+
+    while next_params is not None:
+        resp, n = create_cases(next_params, n, CaseClass)
+        next_params = json.loads(resp.content)['meta']['next']
+
+    print(f"{n} records processed.")
+
+if __name__ == '__main__':
+    print("Creating job openings...")
+    import_cases('job-opening', JobOpening)
+    print("Creating job seekers...")
+    import_cases('job-seeker', JobSeeker)
