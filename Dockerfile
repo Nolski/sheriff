79ca9be0a9a43e5855e0ee608d4e439ee55588e0
Mike Nolan <me@michael-nolan.com>
2018-09-24 17:37:18 +0300

    Add docker-compose setup for webserver

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 # Use an official Python runtime as a parent image
-FROM frolvlad/alpine-python-machinelearning
+FROM python:3
 
 COPY ./ /app
 
 RUN pip install -r /app/requirements.txt
+WORKDIR "/app/src/"
 
-CMD python3 /app/src/app.py
+# CMD python3 -m http.server
+CMD gunicorn --reload -b '0.0.0.0:8000' app
--- src/app.py
+++ src/app.py
@@ -1,25 +1,34 @@
-from apistar import App, Route, http
-from apistar.types import Type
-from apistar.validators import String
+import falcon
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+from mongoengine import connect
+
+connect('sheriff', host='mongodb', port=27017)
+
+COMMCARE_USERNAME = 'michael.nolan@rescue.org'
+COMMCARE_PASSWORD = 'SUPER SECRET'
+CASES_URL = 'https://www.commcarehq.org/a/billy-excerpt/api/v0.5/case/'
+
+class Resource(object):
+
+    def on_get(self, req, resp):
+        doc = {
+            'hello': 'world',
+        }
+
+        cases = requests.get(CASES_URL, auth=HTTPBasicAuth(COMMCARE_USERNAME, COMMCARE_PASSWORD))
+
+        # Create a JSON representation of the resource
+        resp.body = json.dumps(doc, ensure_ascii=False)
+
+        # The following line can be omitted because 200 is the default
+        # status returned by the framework, but it is included here to
+        # illustrate how this may be overridden as needed.
+        resp.status = falcon.HTTP_200
 
 
-class WelcomeParams(Type):
-    name = String(allow_null=True, description="Your name!")
+api = application = falcon.API()
 
-def welcome(params:http.QueryParams) -> dict:
-    name = dict(WelcomeParams(**dict(params)))['name']
-    print(name)
-    if name is None:
-        return {'message': 'Welcome to API Star!'}
-    return {'message': 'Welcome to API Star, %s!' % name}
-
-
-routes = [
-    Route('/', method='GET', handler=welcome),
-]
-
-app = App(routes=routes)
-
-
-if __name__ == '__main__':
-    app.serve('127.0.0.1', 5000, debug=True)
+hello_resource = Resource()
+api.add_route('/', hello_resource)
